fb3a07a6a9779fb282e29db3db84e3c09e241dc7
Tanguy Goretti <tanguy.goretti@gmail.com>
2012-12-04 17:13:08 +0100

    Detail

--- app/controllers/pickup_requests_controller.rb
+++ app/controllers/pickup_requests_controller.rb
@@ -2,7 +2,7 @@ class PickupRequestsController < ApplicationController
 
   def create
     respond_to do |format|
-      format.json { render :json => {:numberOfDrivers => 3} }
+      format.json { render :json => {:numberOfDrivers => 1 + rand(4)} }
     end
   end
 end
