a2826b3c88714c35d6a52361d58d4272250c395a
Tanguy Goretti <tanguy.goretti@gmail.com>
2012-12-06 14:40:27 +0100

    Change api

--- app/controllers/pickup_requests_controller.rb
+++ app/controllers/pickup_requests_controller.rb
@@ -1,8 +1,15 @@
 class PickupRequestsController < ApplicationController
+  def show
+    respond_to do |format|
+      format.json { render :json => { :status => "waiting for drivers", :contactedDriverCount => rand(10) } }
+    end
+  end
 
   def create
     respond_to do |format|
-      format.json { render :json => {:numberOfDrivers => 1 + rand(4)} }
+      format.json { render :json => {:pickupRequestUID => rand(1000000)} }
     end
   end
+
+
 end
